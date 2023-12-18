4e8690e44875c9ef190c3095b217ae8a822288e6
Oleg-Lucenko <lucenko5577@gmail.com>
2023-12-18 17:50:04 +0200

    HW-7, Lucenko

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+interface DataTemplate1 {
+    [item: number | string]: number;
+};
+
+interface DataTemplate2 {
+    [item: string]: ((a: any) => void) | ((a: any) => number);
+};
+
+interface DataTemplate3 {
+    [item: number]: string;
+};
+
+interface DataTemplate4 {
+    description: string,
+    [additionalProperty: number]: string | number | boolean
+};
+
+interface DataTemplate5 {
+    [item: string]: string | number;
+};
+
+interface PossibleVariant5 extends DataTemplate5 {
+    description: string;
+};
+
+interface ObjectWidthIndexSignature  {
+    [key: number]: any
+}
+
+function checkKeysNumbers(obj: ObjectWidthIndexSignature) {
+    let allKeys: any[] = Object.keys(obj);
+    let nanKeys: any[] = allKeys.filter(key => isNaN(Number(key)));
+    return (nanKeys.length === 0 ? 'Keys of this object are only numbers' : `This keys are not a numbers: ${[nanKeys]}`);
+};
+
+
+
+
+
